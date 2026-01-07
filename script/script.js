2185efcd15020e3ba67d107ed407ce73e3c73d26
CRYSANTUS <andrybudiman19@gmail.com>
2026-01-07 12:27:08 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,14 @@
+console.log("Website Materi Perkuliahan Siap Digunakan");
+
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        const href = this.getAttribute("href");
+        if (href && !href.startsWith("#")) {
+            e.preventDefault();
+            document.body.style.opacity = 0;
+            setTimeout(() => {
+                window.location.href = href;
+            }, 300);
+        }
+    });
+});
